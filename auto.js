0164327a7e166ceacf143b21e6fa4083260bd843
Eldhose John <eldhosejohn@users.noreply.github.com>
2017-05-24 20:07:39 +0530

    Added tampermonkey script

---
+++ auto.js
@@ -0,0 +1,33 @@
+// ==UserScript==
+// @name         auto load
+// @namespace http://eldhosejohn.com
+// @include https://console.aws.amazon.com/datapipeline/
+// @include https://console.aws.amazon.com/datapipeline/*
+// @version      1.0
+// @description  Removes the dark menubar from GitHub.
+// @author       eldhosejohn
+// @grant        none
+
+// ==/UserScript==
+
+(function() {
+    'use strict';
+    setTimeout(function(){
+        document.getElementsByClassName("GND1IDUBD GND1IDUBO GND1IDUBK GND1IDUBNK")[0].click();
+        }, 2000);
+    setTimeout(function(){
+        document.getElementsByClassName("GND1IDUBD GND1IDUBO GND1IDUBK GND1IDUBNK")[0].click();
+    }, 2500);
+    setTimeout(function(){
+        document.getElementsByClassName("GND1IDUBD GND1IDUBO GND1IDUBK GND1IDUBNK")[0].click();
+    }, 3000);
+    setTimeout(function(){
+        document.getElementsByClassName("GND1IDUBD GND1IDUBO GND1IDUBK GND1IDUBNK")[0].click();
+    }, 3500);
+    setTimeout(function(){
+        document.getElementsByClassName("GND1IDUBD GND1IDUBO GND1IDUBK GND1IDUBNK")[0].click();
+    }, 4000);
+    setTimeout(function(){
+        document.getElementsByClassName("GND1IDUBD GND1IDUBO GND1IDUBK GND1IDUBNK")[0].click();
+    }, 4400);
+})();
